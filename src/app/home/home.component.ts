4bf5920e985c7f56644c5b18d9dd6b91bfc9e259
manish sharma <smanish2401@gmail.com>
2023-12-02 21:07:06 +0530

    make home page layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,14 +9,18 @@ import { product } from '../data-types';
 })
 export class HomeComponent {
 popularProducts: undefined | product[]
+isLoading:boolean=false;
   constructor(private product:ProductsService) {}
 
   ngOnInit():void{
-
+    this.isLoading=true
     this.product.popularProducts().subscribe((data)=>{
       if(data){
         console.log(`popular products ${data}`)
         this.popularProducts=data
+        setTimeout(() => {
+          this.isLoading=false
+        },1000);
       }
     })
   }
